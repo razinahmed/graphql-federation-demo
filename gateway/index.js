94b2730dd17799e14e8dca319770f44b279aa223
Razin Ahmed <razinahmedv@gmail.com>
2026-04-08 13:26:02 +0400

    feat: Scaffolding complete robust architectural system

---
+++ gateway/index.js
@@ -0,0 +1,15 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'users', url: 'http://localhost:4001/graphql' },
+    { name: 'products', url: 'http://localhost:4002/graphql' },
+    { name: 'reviews', url: 'http://localhost:4003/graphql' }
+  ]
+});
+
+const server = new ApolloServer({ gateway, subscriptions: false });
+server.listen(4000).then(({ url }) => {
+  console.log(`🚀 Gateway ready at ${url}`);
+});
---
+++ services/users/schema.js
@@ -0,0 +1,11 @@
+const { gql } = require('apollo-server');
+exports.typeDefs = gql`
+  extend type Query {
+    me: User
+  }
+  type User @key(fields: "id") {
+    id: ID!
+    name: String
+    username: String
+  }
+`;
